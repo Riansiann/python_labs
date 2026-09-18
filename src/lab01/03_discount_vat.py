0bf93c138dffe976b71776099da40b161b6b7537
Riansiann <ekaterinashiriraeva707@gmail.com>
2026-09-18 08:58:40 +0300

    03 update

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -6,5 +6,7 @@ base = price * (1 - discount/100)
 vat_amount = base * (vat/100)
 total = base + vat_amount
 
-print(f'База после скидки: {base:.2f} ₽\nНДС: {vat_amount:.2f} ₽\nИтого к оплате: {total:.2f} ₽')
+print(f"{'База после скидки:':} {base:.2f} ₽") 
+print(f"{'НДС:':<18} {vat_amount:.2f} ₽")
+print(f"{'Итого к оплате:':<18} {total:.2f} ₽")
 
