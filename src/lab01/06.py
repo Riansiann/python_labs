da863f9591d8c6cb5a3affaa9cf03c8c1000ac77
Riansiann <ekaterinashiriraeva707@gmail.com>
2026-09-18 09:35:35 +0300

    06 update

--- src/lab01/06.py
+++ src/lab01/06.py
@@ -1,9 +1,10 @@
-n = int(input())
+n = int(input("in_1: "))
 a = []
 cntT = 0
 cntF = 0
 for i in range(n):
-    a.append(input())
+    value = input(f"in_{i + 2}: ")
+    a.append(value)
     if a[i].split()[-1] == 'True':
         cntT += 1
     else:
